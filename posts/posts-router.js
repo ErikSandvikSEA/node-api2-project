de1ad2be904994e4a303ec3ef9ee5d33532ec49d
eriksandviksea <eriksandvikSEA@Gmail.com>
2020-06-02 11:52:32 -0700

    added GET to '/:id'

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -4,6 +4,8 @@ const Posts = require('../data/db')
 
 const router = express.Router()
 
+
+//GETs
 router.get('/', (req, res) => {
      Posts.find(req.query)
           .then(posts => {
@@ -12,8 +14,23 @@ router.get('/', (req, res) => {
           .catch(error => {
                console.log(error)
                res.status(500).json({
-                    message: 'Error retrieving the posts'
+                    error: "The posts information could not be retrieved."
                })
+          })
+})
+
+router.get('/:id', (req, res) => {
+     Posts.findById(req.params.id)
+          .then(post => {
+               if(post) {
+                    res.status(200).json(post)
+               } else if (!post.id) {
+                    res.status(404).json({ message: "The post with the specified ID does not exist." })
+               }
+          })
+          .catch(error => {
+               console.log(error)
+               res.status(500).json({ error: "The post information could not be retrieved." })
           })
 })
 
